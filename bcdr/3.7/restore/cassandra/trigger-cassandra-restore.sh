6a5fa62c52cd377c053cb0e286b550a904e6744c
Ranjeet Singh <ranje112@in.ibm.com>
2023-03-27 13:56:00 +0530

    Added 3.7 release related artifacts for cp4waiops bcdr

---
+++ bcdr/3.7/restore/cassandra/trigger-cassandra-restore.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# IBM Confidential
+# 5737-M96
+# (C) Copyright IBM Corporation 2021-2023 All Rights Reserved.
+# 
+# 
+#
+
+source $WORKDIR/restore/cassandra/cassandra-utils.sh
+
+cassandrarestore(){
+    
+    ## TODO: Remove below commented code once new code changes are tested
+    # manual_input=$(cat backup-timestamp.json | jq -r .manualinput)
+    # if [[ "$manual_input" == 'false' ]]
+    # then
+	# echo "Updating then backup timestamp for cassandra with latest available backup tar"
+    #     backup_timestamp=$( oc get cm cassandra-bcdr-config -o json -n $namespace | jq -r '.data.backupTimestamp' )
+    #     jq -c --arg backup_timestamp "$backup_timestamp" '.backuptimestamp = $backup_timestamp' backup-timestamp.json > tmp.$$.json && mv tmp.$$.json backup-timestamp.json
+    # fi
+
+    # backuptimestamp=$(cat backup-timestamp.json | jq .backuptimestamp)
+
+    cassandrapods=$(oc get pods -n $namespace --field-selector=status.phase=Running  --no-headers=true --output=custom-columns=NAME:.metadata.name | grep aiops-topology-cassandra-[0-9])
+    keyspaces=$(cat $WORKDIR/restore/cassandra/cassandra-keyspace.json | jq .keyspaces[].name)
+
+    podTimestampMap=$(oc get cm cassandra-bcdr-config -o jsonpath='{.data.cassandra-config-data-map\.json}' -n $namespace)
+    echo "[INFO] $(date) Cassandra Pods and Backup timestamp map: $podTimestampMap"
+
+    restoreCheckValue=0
+    for keyspace in $keyspaces; do
+	if [ $restoreCheckValue -ne 0 ]; then
+          break
+	fi
+        for pod in $cassandrapods; do
+            # Retrieving backup timestamp from cassandra-bcdr-config configmap
+            backuptimestamp=$(jq -r --arg k $pod '.[$k]' <<< "$podTimestampMap")
+            echo "[INFO] $(date) Restoring the keyspace $keyspace on pod $pod"
+            kPodLoop $pod "/opt/ibm/backup_scripts/restore_cassandra.sh -k $keyspace  -t $backuptimestamp -u \${CASSANDRA_USER} -p \${CASSANDRA_PASS} -f"
+	    if [ $? -ne 0 ]; then
+              echo "[ERROR] $(date) Failed to restore keyspace $keyspace on pod $pod, hence aborting the Cassandra restore operation"
+              restoreCheckValue=1
+	      break
+	    fi
+        done
+    done
+}
+
+cassandrarestore
