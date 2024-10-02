844b137f88c96ade833e27357b5e266db0a66729
caleb <caleb@calebs-mbp-2.austin.ibm.com>
2024-10-02 11:57:55 -0500

    fix embedded cognos
    Signed-off-by: caleb <caleb@calebs-MBP-2.attlocal.net>

--- aiops-cognos-analytics/widgets/installWidgets.sh
+++ aiops-cognos-analytics/widgets/installWidgets.sh
@@ -13,22 +13,25 @@
 # -- Before running this script, perform the following:
 # --   Have your Cognos server URL and API key available
 # 
-# --   Usage: installWidgets.sh -u cognos_url [-k cognos_api_key]
+# --   Usage: installWidgets.sh -u cognos_url [-k cognos_api_key] [-e]
 # --------------------------------------------------------------------------------
 args=$*
 url=
 apiKey=
 sessionKey=
 zipFile=
+cpdRoute=
+aiopsNamespace=
+authHeader=
 
 function usage() {
-  echo "Usage: $0 -u cognos_url [-k cognos_api_key]" 1>&2
+  echo "Usage: $0 -u cognos_url [-k cognos_api_key] [-e]" 1>&2
   echo
   exit 1
 }
 
 function prereqCheck() {
-  vars=$(getopt u:k: $args)  
+  vars=$(getopt u:k:e $args)  
   set -- $vars
   while :; do
     case "$1" in
@@ -39,6 +42,10 @@ function prereqCheck() {
     -k)
       apiKey=$2
       shift; shift
+      ;;
+    -e)
+      embedded=true
+      shift
       ;;
     --) shift; 
       break 
@@ -59,9 +66,45 @@ function prereqCheck() {
 
   echo "Checking prereqs ..."
 
+  if [[ ! -z $embedded ]]; then
+    type kubectl > /dev/null 2>&1
+    if [ $? -gt 0 ]; then
+      echo "kubectl command not found. Make sure this is in your PATH."
+      echo
+      exit 1
+    fi
+
+    kubectl auth can-i "*" "*" <<< test > /dev/null 2>&1
+    if [ $? -gt 0 ]; then
+      echo "Authenticate with the Cloud Pak for AIOps cluster using kubectl as an admin user."
+      echo
+      exit 1
+    fi
+
+    aiopsNamespace=$(kubectl get subscriptions.operators.coreos.com -A 2>/dev/null | grep ibm-aiops-orchestrator | awk '{print $1}')
+    if [ -z $aiopsNamespace ]; then
+      echo "Cloud Pak for AIOps namespace not found. Make sure you're logged into the right cluster and have access to this namespace"
+      echo
+      exit 1
+    fi
+
+    cpdRoute="https://$(kubectl get route cpd -n ${aiopsNamespace} -o jsonpath={.spec.host} 2>/dev/null)"
+    if [ -z $cpdRoute ]; then
+      echo "Cloud Pak for AIOps route not found. Make sure you're logged into the right cluster and have access to the AIOps namespace."
+      echo
+      exit 1
+    fi
+
+    adminUser=$(kubectl -n ${aiopsNamespace} get secret platform-auth-idp-credentials -o jsonpath={.data.admin_username} | base64 -d)
+    adminPass=$(kubectl -n ${aiopsNamespace} get secret platform-auth-idp-credentials -o jsonpath={.data.admin_password} | base64 -d)
+    accessToken=$(curl -k -H "username: ${adminUser}" -H "password: ${adminPass}" ${cpdRoute}/v1/preauth/validateAuth 2> /dev/null | jq -r '.accessToken')
+    authHeader="Authorization: Bearer ${accessToken}"
+    url=$url/cognosanalytics/${aiopsNamespace}
+  fi
+
   nodeVer=$(node -v | grep "^v18" 2>&1)
   if [ -z $nodeVer ]; then
-    echo "Node.js v18 is required."
+    echo "Node.js v18+ is required."
     echo
     exit 1
   fi
@@ -72,16 +115,16 @@ function loginCognos() {
   echo "Logging into Cognos server $url ..."
   if [ -z $apiKey ]; then
     echo "Without an API key, anonymous Cognos login will be used."
-    sessionKey=$(curl -k $url/api/v1/session 2>/dev/null | jq -r ".session_key" 2>/dev/null)
+    sessionKey=$(curl -k $url/api/v1/session -H "$authHeader" 2>/dev/null | jq -r ".session_key" 2>/dev/null)
   else
-    sessionKey=$(curl -k -X PUT $url/api/v1/session -d "{\"parameters\":[{\"name\":\"CAMAPILoginKey\",\"value\":\"$apiKey\"}]}" -H "Content-Type: application/json" 2>/dev/null | jq -r ".session_key" 2>/dev/null)
+    sessionKey=$(curl -k -X PUT $url/api/v1/session -d "{\"parameters\":[{\"name\":\"CAMAPILoginKey\",\"value\":\"$apiKey\"}]}" -H "Content-Type: application/json" -H "$authHeader" 2>/dev/null | jq -r ".session_key" 2>/dev/null)
   fi
   if [[ $sessionKey = "null" ]] || [[ -z $sessionKey ]]; then
     echo
     if [ -z $apiKey ]; then
-      echo "Error logging into Cognos server $url. Check the URL, and ensure anonymous access is enabled."
+      echo "Error logging into Cognos server $url. Check the URL, and ensure anonymous access is enabled. If Cognos is embedded within AIOps, use the -e option."
     else
-      echo "Error logging into Cognos server $url. Check the URL and API key, and ensure the server is running."
+      echo "Error logging into Cognos server $url. Check the URL and API key, and ensure the server is running. If Cognos is embedded within AIOps, use the -e option."
     fi
     exit 1
   fi
@@ -105,7 +148,7 @@ function buildWidgets() {
 
 function installWidgets() {
   echo "Installing AIOps widgets ..."
-  message=$(curl -k -X POST $url/api/v1/extensions -H "Content-Type: application/zip" -H "IBM-BA-Authorization: $sessionKey" --data-binary @$zipFile 2>/dev/null | jq -r ".message")
+  message=$(curl -k -X POST $url/api/v1/extensions -H "Content-Type: application/zip" -H "IBM-BA-Authorization: $sessionKey" -H "$authHeader" --data-binary @$zipFile 2>/dev/null | jq -r ".message")
   ret=$?
   if [[ ! -z $message ]] && [[ $message != "null" ]]; then
     echo $message
